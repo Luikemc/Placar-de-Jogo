08b8279c38c5840c1abb9f8532fa8433f2e016bd
Luiz Henrique Martins Chaves <102527990+Luikemc@users.noreply.github.com>
2023-01-28 10:56:28 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,22 +1,22 @@
-var placa1 = 0;
-var placa2 = 0;
+var pr1 = 0;
+var pr2 = 0;
 
 function incrementClick1() {
-    updatePlacar1(++placa1);
+    updatePlacar1(++pr1);
 }
 function incrementClick2() {
-    updatePlacar2(++placa2);
+    updatePlacar2(++pr2);
 }
 function resetCounter() {
-    placa1 = 0;
-    placa2 = 0;
-    updatePlacar1(placa1);
-    updatePlacar2(placa2);
+    pr1 = 0;
+    pr2 = 0;
+    updatePlacar1(pr1);
+    updatePlacar2(pr2);
 }
 
 function updatePlacar1(val) {
-    document.getElementById("placar1").innerHTML = val;
+    document.getElementById("pr1").innerHTML = val;
 }
 function updatePlacar2(val) {
-    document.getElementById("placar2").innerHTML = val;
+    document.getElementById("pr2").innerHTML = val;
 }
